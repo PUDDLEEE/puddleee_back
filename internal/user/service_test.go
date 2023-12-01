22e538fa2f57017484f32ca00df113963a46d94e
milkymilky0116 <sjlee990129@gmail.com>
2023-12-01 16:10:26 +0900

    feat #12 Update Profile Test (Service)

--- internal/user/service_test.go
+++ internal/user/service_test.go
@@ -11,14 +11,14 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestUserService_createUser(t *testing.T) {
+func TestUserService_CreateUser(t *testing.T) {
 	tests := []struct {
 		name   string
 		before func(*testing.T, *mocks.IUserRepository, *ent.Client, context.Context)
 		expect func(*testing.T, *UserService, *ent.Client, context.Context)
 	}{
 		{
-			name: "createUser/create_user_failed",
+			name: "CreateUser/create_user_failed",
 			before: func(t *testing.T, repo *mocks.IUserRepository, client *ent.Client, ctx context.Context) {
 				repo.On("Create",
 					ctx,
@@ -52,7 +52,7 @@ func TestUserService_createUser(t *testing.T) {
 	}
 }
 
-func TestUserService_findOneUser(t *testing.T) {
+func TestUserService_FindOneUser(t *testing.T) {
 	tests := []struct {
 		name   string
 		before func(*testing.T, *mocks.IUserRepository, *ent.Client, context.Context)
@@ -71,6 +71,48 @@ func TestUserService_findOneUser(t *testing.T) {
 			},
 			expect: func(t *testing.T, service *UserService, client *ent.Client, ctx context.Context) {
 				user, err := service.FindOneUser(1)
+
+				require.Error(t, err)
+				require.Nil(t, user)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+			require.NoError(t, err)
+			defer client.Close()
+			require.NoError(t, client.Schema.Create(context.Background()))
+			repo := mocks.NewIUserRepository(t)
+
+			userService := NewService(repo, context.Background(), client)
+			tt.before(t, repo, client, context.Background())
+			tt.expect(t, userService, client, context.Background())
+		})
+	}
+}
+
+func TestUserService_UpdateUser(t *testing.T) {
+	tests := []struct {
+		name   string
+		before func(*testing.T, *mocks.IUserRepository, *ent.Client, context.Context)
+		expect func(*testing.T, *UserService, *ent.Client, context.Context)
+	}{
+		{
+			name: "UpdateUser/update_user_failed",
+			before: func(t *testing.T, repo *mocks.IUserRepository, client *ent.Client, ctx context.Context) {
+				repo.On("Update",
+					ctx,
+					client,
+					1,
+					dto.UpdateUserDTO{},
+				).
+					Return(nil, errors.New("")).
+					Once()
+			},
+			expect: func(t *testing.T, service *UserService, client *ent.Client, ctx context.Context) {
+				user, err := service.UpdateUser(1, dto.UpdateUserDTO{})
 
 				require.Error(t, err)
 				require.Nil(t, user)
